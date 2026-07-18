49e003cb540b548492bdc2780566798f8cf6ced4
Samadhan Ghotekar <samadhanghotekar05@gmail.com>
2026-07-18 22:14:18 +0530

    Add files via upload

---
+++ InheritanceDemo.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.bark();
+    }
+}
